3319de54b1723dd0868de8fce971c47e55a40fc0
xiaohanc <xiao.hanc@husky.neu.edu>
2017-06-30 18:47:39 +0000

    2nd commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const PORT_NUMBER=8080
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(PORT_NUMBER, function () {
+  console.log('Example app listening on port '+PORT_NUMBER+'!')
+})
+
