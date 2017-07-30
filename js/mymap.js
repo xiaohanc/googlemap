045f59d16aad9e848565bc62d1b418e29255c695
xiaohanc <xiao.hanc@husky.neu.edu>
2017-07-30 04:43:48 +0000

    Make code not depend on google maps api

--- js/mymap.js
+++ js/mymap.js
@@ -1,35 +1,38 @@
 var DEFAULT_ZOOM = 15;
 var GOOGLE_API_KEY = 'AIzaSyDFDg1ruZ4GQqPHMv1Z4Lprn_2wXyd0NYk';
 
+$(function() {
+  function initMap() {
+    var position = {
+      lat: 42.43882,
+      lng: -71.02470
+    };
 
-function initMap() {
-  var position = {
-    lat: 42.43882,
-    lng: -71.02470
-  };
+    var map = new google.maps.Map($('#map')[0], {
+      zoom: DEFAULT_ZOOM,
+      center: position
+    });
+    var marker = new google.maps.Marker({
+      position: position,
+      map: map
+    });
 
-  var map = new google.maps.Map($('#map')[0], {
-    zoom: DEFAULT_ZOOM,
-    center: position
-  });
-  var marker = new google.maps.Marker({
-    position: position,
-    map: map
-  });
+    $.ajax({
+      url: '/nearby_search',
+      data: {
+        'location': position.lat + ',' + position.lng,
+        'type': 'restaurant',
+        'key': GOOGLE_API_KEY,
+        'radius': 500
+      },
+      success: function(data) {
+        debugger;
+      },
+      failure: function(data) {
+        debugger;
+      }
+    });
+  }
 
-  $.ajax({
-    url: '/nearby_search',
-    data: {
-      'location': position.lat + ',' + position.lng,
-      'type': 'restaurant',
-      'key': GOOGLE_API_KEY,
-      'radius': 1000
-    },
-    success: function(data) {
-      debugger;
-    },
-    failure: function(data) {
-      debugger;
-    }
-  });
-}
+  initMap();
+});
