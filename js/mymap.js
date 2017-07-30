a42d7ec8fda1f35e21734f000be5b6e006e0e54b
xiaohanc <xiao.hanc@husky.neu.edu>
2017-07-30 03:42:08 +0000

    Get #map with jquery

--- js/mymap.js
+++ js/mymap.js
@@ -1,6 +1,6 @@
 function initMap() {
   var uluru = {lat: 42.43882, lng:  -71.02470};
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map($('#map')[0], {
     zoom: 4,
     center: uluru
   });
