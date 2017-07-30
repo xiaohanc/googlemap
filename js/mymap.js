411a896ef2accf75a658f632772ec324078a0f05
xiaohanc <xiao.hanc@husky.neu.edu>
2017-07-30 03:59:16 +0000

    Do nearby search on the fronend

--- js/mymap.js
+++ js/mymap.js
@@ -1,11 +1,35 @@
+var DEFAULT_ZOOM = 15;
+var GOOGLE_API_KEY = 'AIzaSyDFDg1ruZ4GQqPHMv1Z4Lprn_2wXyd0NYk';
+
+
 function initMap() {
-  var uluru = {lat: 42.43882, lng:  -71.02470};
+  var position = {
+    lat: 42.43882,
+    lng: -71.02470
+  };
+
   var map = new google.maps.Map($('#map')[0], {
-    zoom: 4,
-    center: uluru
+    zoom: DEFAULT_ZOOM,
+    center: position
   });
   var marker = new google.maps.Marker({
-    position: uluru,
+    position: position,
     map: map
+  });
+
+  $.ajax({
+    url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
+    data: {
+      'location': position.lat + ',' + position.lng,
+      'type': 'restaurant',
+      'key': GOOGLE_API_KEY,
+      'radius': 1000
+    },
+    success: function(data) {
+      debugger;
+    },
+    failure: function(data) {
+      debugger;
+    }
   });
 }
